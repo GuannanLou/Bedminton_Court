adb60fe0db9fac554d26c35bd62b3d60a17395b9
GuannanLou <117661132+GuannanLou@users.noreply.github.com>
2025-02-13 15:59:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,26 +5,32 @@ import hashlib
 import base64
 import os
 
-if __name__ == '__main__':
+def get_img(img_path):
+    with open(img_path, 'rb') as f:
+        im_bytes = f.read()
+        im_b64 = str(base64.b64encode(im_bytes), "utf-8")
+    with open(img_path, 'rb') as f:
+        im_fd = f.read()
+        md = hashlib.md5()
+        md.update(im_fd)
+        im_md5 = md.hexdigest()
+    return im_b64, im_md5
+
+def get_court(content, today):
     content = ""
     content += "正在查询晚上7点之后的场次\n"
 
     # 获取今天的日期
-    today = datetime.today().date()
-    # print("起始日期:", today)
     content += "起始日期: {}\n".format(today)
 
     # 计算七天后的日期
     seven_days_later = today + timedelta(days=6)
-    # print("终止日期:", seven_days_later)
     content += "终止日期: {}\n".format(seven_days_later)
 
-    # print('\r开始查询场地')
     link = 'https://api.sport.sheffield.ac.uk/api/site/HO/activitygroup/BAD/activity/BADTN/slots'
     # link = 'https://api.sport.sheffield.ac.uk/api/site/HO/activitygroup/GSC-MUGA/activity/GSC-SMUGA-TEN/slots'
-    url = '{}?plus2Id=0&start={}T00%3A00%3A00.000Z&end={}T00%3A00%3A00.000Z&groupResources=true'.format(link, today,
-                                                                                                        seven_days_later)
-    # print(url)
+    url = '{}?plus2Id=0&start={}T00%3A00%3A00.000Z&end={}T00%3A00%3A00.000Z&groupResources=true'.format(link, today, seven_days_later)
+
     courts = requests.get(url).text
     courts = courts.replace('false', 'False')
     courts = courts.replace('true', 'True')
@@ -38,21 +44,12 @@ if __name__ == '__main__':
         if court['available'] == 0: filted = True
         if not filted:
             filted_courts.append(court)
+    return content, filted_courts
 
+def get_court_text(today):
+    content, filted_courts = get_court(content, today)
+    
     weekday_dict = ['Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat', 'Sun']
-
-    # image base64 and md5
-    # image = 'https://github.com/GuannanLou/Bedminton_Court/res/profile.png'
-    # image = 'https://raw.githubusercontent.com/GuannanLou/Bedminton_Court/refs/heads/master/res/profile.png'
-    image = './res/profile.png'
-    with open(image, 'rb') as f:
-        im_bytes = f.read()
-        im_b64 = str(base64.b64encode(im_bytes), "utf-8")
-    with open(image, 'rb') as f:
-        im_fd = f.read()
-        md = hashlib.md5()
-        md.update(im_fd)
-        im_md5 = md.hexdigest()
 
     # print('\r查询结果:')
     content += "查询结果:"
@@ -78,22 +75,29 @@ if __name__ == '__main__':
         content += ' 叫我来干啥\n'
         content += ' hei不如去攀岩'
 
-    # print()
-    # print(content)
+    return content
 
-    bot_key = os.getenv("BOT_API_KEY")
+def send_text(url, content):
+    msg = {"msgtype": "text", "text": {"content": content}}
+    res_text = requests.post(url=url, data=json.dumps(msg))
+    return res_text
 
-    # send text
-    url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={}".format(bot_key.replace('_', '-'))
-    data = {
-        "msgtype": "text",
-        "text": {
-            "content": content,
-        }
-    }
-    res_text = requests.post(url=url, data=json.dumps(data))
-    # print(res.text)
-    # send image
+def send_img(url, image):
+    im_b64, im_md5 = get_img(image)
+    
     headers = {"content-type": "application/json"}
     msg = {"msgtype": "image", "image": {"base64": im_b64, "md5": im_md5}}
     res_image = requests.post(url=url, headers=headers, json=msg)
+    return res_image
+
+if __name__ == '__main__':
+    bot_key = os.getenv("BOT_API_KEY")
+    url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={}".format(bot_key.replace('_', '-'))
+
+    today = datetime.today().date()
+    content = get_court_text(today)
+
+    image = './res/profile.png'
+
+    send_text(url, content)
+    send_img(url, image)
