c9b97c0a52c27fa160a822313c86d1458b5ec23a
GuannanLou <117661132+GuannanLou@users.noreply.github.com>
2025-02-18 12:20:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -137,6 +137,74 @@ def send_img(url, image):
     res_image = requests.post(url=url, headers=headers, json=msg)
     return res_image
 
+def send_card(url):
+    msg = {
+        "msgtype":"template_card",
+        "template_card":{
+            "card_type":"text_notice",
+            "source":{
+                "icon_url":"https://wework.qpic.cn/wwpic/252813_jOfDHtcISzuodLa_1629280209/0",
+                "desc":"企业微信",
+                "desc_color":0
+            },
+            "main_title":{
+                "title":"欢迎使用企业微信",
+                "desc":"您的好友正在邀请您加入企业微信"
+            },
+            "emphasis_content":{
+                "title":"100",
+                "desc":"数据含义"
+            },
+            "quote_area":{
+                "type":1,
+                "url":"https://work.weixin.qq.com/?from=openApi",
+                "appid":"APPID",
+                "pagepath":"PAGEPATH",
+                "title":"引用文本标题",
+                "quote_text":"Jack：企业微信真的很好用~\nBalian：超级好的一款软件！"
+            },
+            "sub_title_text":"下载企业微信还能抢红包！",
+            "horizontal_content_list":[
+                {
+                    "keyname":"邀请人",
+                    "value":"张三"
+                },
+                {
+                    "keyname":"企微官网",
+                    "value":"点击访问",
+                    "type":1,
+                    "url":"https://work.weixin.qq.com/?from=openApi"
+                },
+                {
+                    "keyname":"企微下载",
+                    "value":"企业微信.apk",
+                    "type":2,
+                    "media_id":"MEDIAID"
+                }
+            ],
+            "jump_list":[
+                {
+                    "type":1,
+                    "url":"https://work.weixin.qq.com/?from=openApi",
+                    "title":"企业微信官网"
+                },
+                {
+                    "type":2,
+                    "appid":"APPID",
+                    "pagepath":"PAGEPATH",
+                    "title":"跳转小程序"
+                }
+            ],
+            "card_action":{
+                "type":1,
+                "url":"https://work.weixin.qq.com/?from=openApi",
+                "appid":"APPID",
+                "pagepath":"PAGEPATH"
+            }
+        }
+    }
+    res_text = requests.post(url=url, msgtype='template_card', template_card=json.dumps(msg))
+    return res_text
 
 if __name__ == '__main__':
     bot_key = os.getenv("BOT_API_KEY")
@@ -149,3 +217,4 @@ if __name__ == '__main__':
 
     send_img(url, image)
     send_text(url, content)
+    send_card(url)
