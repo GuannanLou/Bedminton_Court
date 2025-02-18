a6c265f8bb214bcb17af5fb79a8277e19b9f03a0
Ruizhen Gu <ravenna1031@gmail.com>
2025-02-18 13:26:43 +0000

    send card test

--- main.py
+++ main.py
@@ -20,22 +20,26 @@ def get_img(img_path):
     return im_b64, im_md5
 
 
-def get_court(content, today):
-    content = ""
-    content += "\n\n正在查询晚上7点之后的场次\n"
+def get_court(today):
+    # content = ""
+    # content += "\n\n正在查询晚上7点之后的场次\n"
+
+    content = {}
 
     # 获取今天的日期
-    content += "起始日期: {}\n".format(today)
+    # content += "起始日期: {}\n".format(today)
+    content["start_date"] = "{}".format(today)
 
     # 计算七天后的日期
     seven_days_later = today + timedelta(days=6)
-    content += "终止日期: {}\n".format(seven_days_later)
+    # content += "终止日期: {}\n".format(seven_days_later)
+    content["end_date"] = "{}".format(seven_days_later)
 
     link = 'https://api.sport.sheffield.ac.uk/api/site/HO/activitygroup/BAD/activity/BADTN/slots'
     # link = 'https://api.sport.sheffield.ac.uk/api/site/HO/activitygroup/GSC-MUGA/activity/GSC-SMUGA-TEN/slots'
     url = '{}?plus2Id=0&start={}T00%3A00%3A00.000Z&end={}T00%3A00%3A00.000Z&groupResources=true'.format(link, today,
                                                                                                         seven_days_later)
-    print(url)
+    # print(url)
 
     courts = requests.get(url).text
     courts = courts.replace('false', 'False')
@@ -66,15 +70,15 @@ def custom_api():
 
 
 def get_court_text(today):
-    content, filted_courts = get_court("", today)
+    content, filted_courts = get_court(today)
 
     weekday_dict = ['Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat', 'Sun']
 
     custom_api_res = custom_api()
 
-    state = False
+    # state = False
 
-    content += "查询结果:\n"
+    # content += "查询结果:\n"
     if len(filted_courts) > 0:
         current = None
         for court in filted_courts:
@@ -85,33 +89,40 @@ def get_court_text(today):
                     weekday_dict[date(int(current.split('-')[0]), int(current.split('-')[1]),
                                       int(current.split('-')[2])).weekday()]
                 )
-            content += ' {}    available: {}\n'.format(
+            # content += ' {}    available: {}\n'.format(
+            #     court['start'].split('T')[1],
+            #     str(court['available'])
+            # )
+            content["booking_detail"] = ' {}    available: {}\n'.format(
                 court['start'].split('T')[1],
                 str(court['available'])
             )
         state = True
     else:
-        content += '\n 这周也没有场子啊🅰\n'
-        content += ' 叫我来干啥\n'
-        content += ' hei不如去攀岩'
-        content += '\n'
+        # content += '\n 这周也没有场子啊🅰\n'
+        # content += ' 叫我来干啥\n'
+        # content += ' hei不如去攀岩'
+        # content += '\n'
+        content["booking_detail"] = '这周也没有场子啊🅰\n hei不如去攀岩'
 
         state = False
 
-    stat_text = '没场，不用订' if not state else random.choice([
+    content["booking_info"] = '没场，不用订' if not state else random.choice([
         '老黑自觉定场',
         '顾顾自觉定场',
         '学弟自觉定场',
         '学姐自觉定场',
     ])
 
-    content += '\n'
-    content += stat_text
+    content["warning"] = custom_api_res
 
-    content += '\n\n警世格言：\n'
-    content += custom_api_res
+    # content += '\n'
+    # content += stat_text
 
-    content = stat_text + content + '\n'
+    # content += '\n\n警世格言：\n'
+    # content += custom_api_res
+
+    # content = stat_text + content + '\n'
 
     return content
 
@@ -129,6 +140,53 @@ def send_text(url, content):
     return res_text
 
 
+def send_card(url, content):
+    msg = {
+        "msgtype": "template_card",
+        "template_card": {
+            "card_type": "text_notice",
+            "main_title": {
+                "title": "羽毛球场次查询",
+                "desc": "您好，我是您的羽毛球场次查询小黑奴"
+            },
+            "sub_title_text": "正在查询晚上7点之后的场次",
+            "horizontal_content_list": [
+                {
+                    "keyname": "起始日期",
+                    "value": content["start_date"]
+                },
+                {
+                    "keyname": "终止日期",
+                    "value": content["end_date"]
+                },
+            ],
+            "emphasis_content": {
+                "title": content["booking_info"],
+                "desc": "点击卡片进入订场页面"
+            },
+            "jump_list": [
+                {
+                    "type": 1,
+                    "url": "https://api.oick.cn/api/dog",
+                    "title": "警世格言"
+                },
+                {
+                    "type": 1,
+                    "url": "https://raw.githubusercontent.com/GuannanLou/Bedminton_Court/refs/heads/master/res/profile.png",
+                    "title": "每日猫图"
+                }
+            ],
+            "card_action": {
+                "type": 1,
+                "url": "https://sport.sheffield.ac.uk/activities/0/HO/BAD/BADTN",
+            }
+        }
+    }
+    headers = {"content-type": "application/json"}
+    res_card = requests.post(url=url, data=json.dumps(msg))
+    return res_card
+
+
 def send_img(url, image):
     im_b64, im_md5 = get_img(image)
 
@@ -137,74 +195,6 @@ def send_img(url, image):
     res_image = requests.post(url=url, headers=headers, json=msg)
     return res_image
 
-def send_card(url):
-    msg = {
-        "msgtype":"template_card",
-        "template_card":{
-            "card_type":"text_notice",
-            "source":{
-                "icon_url":"https://wework.qpic.cn/wwpic/252813_jOfDHtcISzuodLa_1629280209/0",
-                "desc":"企业微信",
-                "desc_color":0
-            },
-            "main_title":{
-                "title":"欢迎使用企业微信",
-                "desc":"您的好友正在邀请您加入企业微信"
-            },
-            "emphasis_content":{
-                "title":"100",
-                "desc":"数据含义"
-            },
-            "quote_area":{
-                "type":1,
-                "url":"https://work.weixin.qq.com/?from=openApi",
-                "appid":"APPID",
-                "pagepath":"PAGEPATH",
-                "title":"引用文本标题",
-                "quote_text":"Jack：企业微信真的很好用~\nBalian：超级好的一款软件！"
-            },
-            "sub_title_text":"下载企业微信还能抢红包！",
-            "horizontal_content_list":[
-                {
-                    "keyname":"邀请人",
-                    "value":"张三"
-                },
-                {
-                    "keyname":"企微官网",
-                    "value":"点击访问",
-                    "type":1,
-                    "url":"https://work.weixin.qq.com/?from=openApi"
-                },
-                {
-                    "keyname":"企微下载",
-                    "value":"企业微信.apk",
-                    "type":2,
-                    "media_id":"MEDIAID"
-                }
-            ],
-            "jump_list":[
-                {
-                    "type":1,
-                    "url":"https://work.weixin.qq.com/?from=openApi",
-                    "title":"企业微信官网"
-                },
-                {
-                    "type":2,
-                    "appid":"APPID",
-                    "pagepath":"PAGEPATH",
-                    "title":"跳转小程序"
-                }
-            ],
-            "card_action":{
-                "type":1,
-                "url":"https://work.weixin.qq.com/?from=openApi",
-                "appid":"APPID",
-                "pagepath":"PAGEPATH"
-            }
-        }
-    }
-    res_text = requests.post(url=url, data=json.dumps(msg))
-    return res_text
 
 if __name__ == '__main__':
     bot_key = os.getenv("BOT_API_KEY")
@@ -212,9 +202,10 @@ if __name__ == '__main__':
 
     today = datetime.today().date()
     content = get_court_text(today)
+    send_card(url, content)
 
-    image = './res/profile.png'
+    # image = './res/profile.png'
 
-    send_img(url, image)
-    send_text(url, content)
+    # send_img(url, image)
+    # send_text(url, content)
     # send_card(url)
